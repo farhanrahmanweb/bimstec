4fd16cb96ea73dfba0f5ff85c5653aecd2f80787
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-02-18 05:08:35 +0600

    Merge Conflict Fixed

--- resources/views/frontend/secretary-general.blade.php
+++ resources/views/frontend/secretary-general.blade.php
@@ -11,6 +11,22 @@
             height: 350px;
             object-fit: cover;
         }
+
+        .card-shadow {
+            box-shadow: 0px 1px 15px #ccc;
+            margin: 1%;
+            border-radius: 10px;
+            overflow: hidden;
+            min-height: 28rem;
+            max-height: 28rem;
+        }
+
+        .card-img-top {
+            width: 100%;
+            height: 16rem;
+            object-fit: cover;
+            object-position: center;
+        }
     </style>
 @endpush
 @section('content')
@@ -46,8 +62,10 @@
                 <div class="col-12 tab-style3">
                     <!-- start tab navigation -->
                     <ul class="nav nav-tabs margin-50px-bottom sm-margin-30px-bottom text-uppercase alt-font text-small font-weight-600 justify-content-center flex-column flex-md-row">
-                        <li class="nav-item"><a class="nav-link active" href="#tab_sec1" data-toggle="tab">Profile</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#tab_sec2" data-toggle="tab">Secretary General's Page</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#tab_sec1" data-toggle="tab">Profile</a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="#tab_sec2" data-toggle="tab">Secretary General's
+                                Page</a></li>
                         <li class="nav-item"><a class="nav-link" href="#tab_sec3" data-toggle="tab">Statements</a></li>
                     </ul>
                     <!-- end tab navigation -->
@@ -55,75 +73,32 @@
             </div>
             <div class="tab-content">
                 <!-- start tab content -->
-                <div class="tab-pane med-text fade in active show profile" id="tab_sec1">
+                <div class="tab-pane med-text fade in active show Profile" id="tab_sec1">
                     <div class="row align-items-center">
-                        <div class="col-12 col-md-12 sm-margin-30px-bottom margin-30px-bottom text-center">
-                            <img src="{{asset('frontend/images/trade-investment-1.jpg')}}" alt="" class="w-100"/>
-                        </div>
-                        <div class="col-12 col-lg-10 col-md-10 offset-lg-1 text-justify">
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                                printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                                publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it
-                                has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
-                                packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                                Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-                                going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-                                Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                                The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-                                going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-                                Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                                The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-                            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
-                                Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-                            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
-                                Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which
-                                don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of
-                                text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a
-                                dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
-                                therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-
-                            <a href="#" class="btn btn-very-small btn-dark-gray text-very-small border-radius-4 lg-margin-15px-bottom d-table d-lg-inline-block md-margin-lr-auto"><i
-                                    class="fab fa-twitter" aria-hidden="true"></i> Follow @profile_person</a>
-                        </div>
+                        {!! $profile->content !!}
                     </div>
                 </div>
                 <!-- end tab content -->
                 <!-- start tab content -->
                 <div class="tab-pane fade in" id="tab_sec2">
                     <div class="row align-items-center">
-                        <div class="col-sm-12">
-                            <table class="table">
-                                <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Date</th>
-                                    <th scope="col">Title</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($secretaryPages as $key=>$secretarypage)
-                                    <tr>
-                                        <th scope="row">{{$key+1}}</th>
-                                        <td>{{\Carbon\Carbon::parse($secretarypage->date, 'UTC')->isoFormat('Do MMM YYYY')}}</td>
-                                        <td>
-                                            <u><a href="{{route('secretary-page', $secretarypage->id)}}">{{\Illuminate\Support\Str::substr($secretarypage->title, 0, 100)}}..</a></u>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                        <div class="row">
+                            @foreach($secretaryPages as $key=>$secretarypage)
+                                <div class="col-md-3">
+                                    <a href="{{route('secretary-page', $secretarypage->id)}}">
+                                        <div class="card p-0 card-shadow">
+                                            <img class="card-img-top"
+                                                 src="{{asset('storage/secretary/'.$secretarypage->file)}}"
+                                                 alt="Card image cap">
+                                            <div class="card-body">
+                                                <h6 class="card-title link">{{$secretarypage->title}}</h6>
+                                                <p class="card-title">{{\Carbon\Carbon::parse($secretarypage->date, 'UTC')->isoFormat('Do MMM YYYY')}}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            @endforeach
+
                         </div>
                     </div>
                 </div>
@@ -132,19 +107,24 @@
                 <div class="tab-pane fade in" id="tab_sec3">
                     <div class="container">
                         @foreach($statements as $key=>$statement)
-                            <div class="row margin-10px-bottom align-items-center">
+                            <div class="row margin-10px-bottom">
                                 <div class="col-12 col-md-2 md-margin-30px-bottom last-paragraph-no-margin">
-                                    <div class="feature-content padding-10px-all bg-white box-shadow-light lg-padding-20px-all text-center">
-                                        <div class="alt-font font-weight-600 text-extra-dark-gray"><span class="text-deep-pink margin-5px-right" style="font-size: 25px;">{{\Carbon\Carbon::parse($statement->date, 'UTC')->isoFormat('Do MMM')}} <br></span> {{\Carbon\Carbon::parse($statement->date, 'UTC')->isoFormat('YYYY')}}
+                                    <div
+                                        class="feature-content padding-10px-all bg-white box-shadow-light lg-padding-20px-all text-center">
+                                        <div class="alt-font font-weight-600 text-extra-dark-gray"><span
+                                                class="text-deep-pink margin-5px-right" style="font-size: 25px;">{{\Carbon\Carbon::parse($statement->date, 'UTC')->isoFormat('Do MMM')}} <br></span> {{\Carbon\Carbon::parse($statement->date, 'UTC')->isoFormat('YYYY')}}
                                         </div>
                                         {{--                                        <p>Danmodi, DHAKA</p>--}}
                                     </div>
+                                </div>
 
-                                <div class="col-12 col-lg-10 text-center text-lg-left">
-                                    <div class="feature-content padding-10px-all bg-white box-shadow-light lg-padding-20px-all download_tb h-100 position-relative">
-                                        <p class="text-medium my-3">{{$statement->title}}</p>
-                                        <a class="btn d-inline-block btn-success btn-small text-extra-small" href="{{asset('storage/secretary/'.$statement->file)}}"
-                                           target="_blank">Download</a>
+                                <div class="col-12 col-lg-10 text-center text-lg-left wow fadeIn">
+                                    <div
+                                        class="feature-content padding-10px-all bg-white box-shadow-light lg-padding-20px-all download_tb px-4">
+                                        <h2 class="text-large line-height-25 text-dark">{{$statement->title}}</h2>
+                                        <a class="text-extra-small line-height-25 text-white btn btn-success"
+                                           href="{{asset('storage/secretary/'.$statement->file)}}"
+                                           target="_blank"><u>Download</u></a>
                                     </div>
                                 </div>
                             </div>
