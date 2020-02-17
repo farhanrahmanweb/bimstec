9239b55b28daafc41c48cb1019abc7a20a426393
farhan <arfarhan44@gmail.com>
2020-02-17 12:50:38 +0600

    New File Added

---
+++ resources/views/frontend/areas-cooperation.blade.php
@@ -0,0 +1,185 @@
+@extends('layouts.frontend.app')
+@section('title', 'Area of Cooperation Bay of Bengal Initiative for Multi-Sectoral Technical and Economic Cooperation')
+@section('content')
+
+    <!-- start page title section -->
+    <section id="page_title" class="wow fadeIn cover-background background-position-center top-space" style="background-image:url('images/page-title-img.JPG');">
+        <div class="opacity-medium bg-extra-dark-gray"></div>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 d-flex flex-column text-center justify-content-center page-title-large padding-30px-tb">
+                    <!-- start page t   itle -->
+                    <h1 class="alt-font text-white font-weight-600 mb-0">Areas of Cooperation</h1>
+                    <!-- start breadcrumb -->
+                    <div class="breadcrumb">
+                        <ul class="text-center text-white text-md-right">
+                            <li><a href="index.php">Home</a></li>
+                            <li>Areas of Cooperation</li>
+                        </ul>
+                    </div>
+                    <!-- end breadcrumb -->
+                    <!-- end page title -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end page title section -->
+    <!-- start section -->
+    <section id="trade" class="wow fadeIn">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-4 text-center text-lg-left md-margin-30px-bottom md-padding-80px-lr sm-padding-15px-lr wow fadeIn">
+                    <h6 class="alt-font font-weight-700 text-extra-dark-gray text-uppercase width-80 lg-width-100">Areas of Cooperation</h6>
+                    <span class="mb-4 separator-line-horrizontal-medium-light2 bg-deep-pink d-table mr-auto width-100px"></span>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 sm-margin-15px-bottom wow fadeIn object-fit" data-wow-delay="0.2s">
+                    <img src="images/trade-investment-1.jpg" alt=""/>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 wow fadeIn object-fit" data-wow-delay="0.4s">
+                    <img src="images/trade-investment-2.jpg" alt=""/>
+                </div>
+
+
+                <div class="col-12 col-lg-12 col-md-12 wow fadeIn" data-wow-delay="0.6s" style="padding-top: 50px;">
+
+                    <h6>BIMSTEC Priority Sectors</h6>
+                    <p>Six priority sectors of cooperation were identified at the 2nd Ministerial Meeting in Dhaka on 19 November 1998. They include the followings:</p>
+
+
+                    <ol>
+                        <li><a href="">Trade & Investment</a></li>
+                        <li><a href="">Transport & Communication</a></li>
+                        <li><a href="">Energy</a></li>
+                        <li><a href="">Tourism</a></li>
+                        <li><a href="">Technology</a></li>
+                        <li><a href="">Fisheries</a></li>
+                        <br>
+
+
+                        <p>After the 8th Ministerial Meeting in Dhaka on 18-19 December 2005, a number of new areas of cooperation emerged. The number of priority sectors of cooperation increased
+                            from 6 to 13. The 7 new sectors were discussed in the 1st BIMSTEC Summit and there has been various activities to enhance those co-operations ever since.The sectors are
+                            as follows;</p>
+
+
+                        <li><a href="">Agriculture</a></li>
+                        <li><a href="">Public Health</a></li>
+                        <li><a href="">Poverty Alleviation</a></li>
+                        <li><a href="">Counter-Terrorism & Transnational Crime</a></li>
+                        <li><a href="">Environment & Disaster Management</a></li>
+                        <li><a href="">People-to-People Contact</a></li>
+                        <li><a href="">Cultural Cooperation</a></li>
+                        <br>
+
+                        <p>The thirteenth Session of the BIMSTEC Senior Officials’ Meeting recommended the inclusion of Climate Change as the 14th priority area of cooperation. The eleventh
+                            BIMSTEC Ministerial Meeting held in New Delhi in November 2008 endorsed this recommendation.</p>
+                        <li><a href="">Climate Change </a></li>
+                    </ol>
+
+
+                    <div class="table-responsive">
+                        <table class="margin-five-top margin-five-bottom table table-striped table-inverse" style="border-bottom: 5px solid #00a751;">
+                            <thead>
+                            <tr>
+                                <th scope="col" style="width:50px" ;>#</th>
+                                <th scope="col" style="width:400px" ;><strong>Sectors</strong></th>
+                                <th scope="col"><strong>Lead Countries</strong></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Trade & Investment/Sub-sector</td>
+                                <td>Bangladesh</td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td>Technology/Sub-sector</td>
+                                <td>Sri Lanka</td>
+                            </tr>
+                            <tr>
+                                <td>3.</td>
+                                <td>Energy/Sub-sector</td>
+                                <td>Myanmar</td>
+                            </tr>
+                            <tr>
+                                <td>4.</td>
+                                <td>Transportation & Communication/Sub-sector</td>
+                                <td>India</td>
+                            </tr>
+                            <tr>
+                                <td>5.</td>
+                                <td>Tourism/Sub-sector</td>
+                                <td>India</td>
+                            </tr>
+                            <tr>
+                                <td>6.</td>
+                                <td>Fisheries/Sub-sector</td>
+                                <td> Thailand</td>
+                            </tr>
+                            <tr>
+                                <td>7.</td>
+                                <td>Agriculture/Sub-sector</td>
+                                <td>Myanmar</td>
+                            </tr>
+
+                            <tr>
+                                <td>8.</td>
+                                <td>Cultural Cooperation/Sub-sector</td>
+                                <td>Bhutan</td>
+                            </tr>
+                            <tr>
+                                <td>9.</td>
+                                <td>Environment and Disaster Management/Sub-sector</td>
+                                <td>India</td>
+                            </tr>
+                            <tr>
+                                <td>10.</td>
+                                <td>Public Health/Sub-sector</td>
+                                <td>Thailand</td>
+                            </tr>
+                            <tr>
+                                <td>11.</td>
+                                <td>People-to-People Contact/Sub-sector</td>
+                                <td>Thailand</td>
+                            </tr>
+                            <tr>
+                                <td>12.</td>
+                                <td>Poverty Alleviation/Sub-sector</td>
+                                <td>Nepal</td>
+                            </tr>
+                            <tr>
+                                <td>13.</td>
+                                <td>Counter-Terrorism and Transnational Crime/Sub-sector</td>
+                                <td>India</td>
+                            </tr>
+                            <tr>
+                                <td>14.</td>
+                                <td>Climate Change</td>
+                                <td>Bangladesh</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <h6>Roles and Functions of Lead Country</h6>
+                    <ol>
+                        <li>Where sub-sectors have been identified, the designated Chair Countries will seek to identify specific projects for facilitating cooperation.</li>
+                        <li>The Chair Country will function in consultation with an Expert Group drawn from member countries.</li>
+                        <li>The Chair Country will chair and coordinate the work of the Expert Group, and perform secretarial functions if the Expert Group in such a way as to ensure the
+                            satisfactory and successful implementation of the projects and to monitor the progress of the work.
+                        </li>
+                        <li>The members of the Expert Group will be in regular contact through correspondence, fax, e-mail, etc. and will meet whenever deemed necessary.</li>
+                        <li>The Expert Group will coordinate, monitor and review progress in projects being implemented and will report on the same to the Sectoral Committee.</li>
+                        <li>The Expert Group may invite such representatives from the public/private sector and regional/international organizations to its meetings whenever deemed appropriate.
+                        </li>
+                        <li>The sub-sector Chair Country position will rotate between members every three years.</li>
+                    </ol>
+
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+    <!-- end section -->
+@endsection
