e97c59741265754bc9a2aa941ee58b002ffa083e
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-02-19 14:06:34 +0600

    Secretary page exception handled

--- app/Http/Controllers/Admin/SecretaryProfileController.php
+++ app/Http/Controllers/Admin/SecretaryProfileController.php
@@ -10,6 +10,7 @@ use Brian2694\Toastr\Facades\Toastr;
 use Cassandra\Bigint;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
@@ -22,7 +23,7 @@ class SecretaryProfileController extends Controller
      */
     public function edit()
     {
-        $secretary = SecretaryProfile::find(1);
+        $secretary = DB::table('secretary_profiles')->first();
         return view('backend.admin.editSecretaryProfile', compact('secretary'));
     }
 
