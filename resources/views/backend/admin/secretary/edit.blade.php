9251cbeb9e41588187961fd12fc4aa3251ec87e7
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-03-04 10:30:18 +0600

    Datepicker fixed

--- resources/views/backend/admin/secretary/edit.blade.php
+++ resources/views/backend/admin/secretary/edit.blade.php
@@ -18,7 +18,7 @@
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">Date</label>
-                                <input id="dates" name="date" value="{{$secretary->date}}" placeholder="Date" class="form-control" autocomplete="false">
+                                <input type="date" id="dates" name="date" value="{{$secretary->date}}" placeholder="Date" class="form-control" autocomplete="false">
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">Type</label>
@@ -64,7 +64,7 @@
         $(function ($) {
             CKEDITOR.replace( 'description', {height: 500});
         })(jQuery)
-        //$(function ($) {
+       // $(function ($) {
         $('#dates').datepicker({
             uiLibrary: 'bootstrap4',
             format: 'yyyy-mm-dd'
